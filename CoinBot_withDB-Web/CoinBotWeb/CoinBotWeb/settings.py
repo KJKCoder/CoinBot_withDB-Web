94ef6dbb69e004f77d9b8a2d8452a5b7e677aa22
KJKcoder <98453133+KJKCoder@users.noreply.github.com>
2022-08-06 17:37:31 +0900

    Update settings.py

--- CoinBot_withDB-Web/CoinBotWeb/CoinBotWeb/settings.py
+++ CoinBot_withDB-Web/CoinBotWeb/CoinBotWeb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7ab*%-cawiq493wmd&0r&jivtx*q+dx)54rb+o+-h@z(tpf%$$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.125.236.219']
 
 
 # Application definition
