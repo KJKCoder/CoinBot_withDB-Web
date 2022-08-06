c3842a05fe3c5e635a4c2774f2812eb4e0316aad
KJKcoder <98453133+KJKCoder@users.noreply.github.com>
2022-08-06 18:13:13 +0900

    Update admin.py

--- CoinBot_withDB-Web/CoinBotWeb/searchData/admin.py
+++ CoinBot_withDB-Web/CoinBotWeb/searchData/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import 날짜, 거래, BTC_ETH
 
 # Register your models here.
+
+admin.site.register(날짜)
+admin.site.register(BTC_ETH)
+admin.site.register(거래)
